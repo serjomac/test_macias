9ccb00a341044fb22ce18a10eec391ff83b077e0
Sergio Macias Lopez <smacias@facilito.com.ec>
2022-05-01 20:54:03 -0500

    Initial Commit

---
+++ prueba-macias/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prueba-macias
+//
+//  Created by Jonathan Macias on 1/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
